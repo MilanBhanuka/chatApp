8e162e6e7e881b2687c5a2738560af48081c4bfe
MilanBhanuka <vgmbhanuka@gmail.com>
2024-09-05 21:53:13 +0530

    assets

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat() {
+  return (
+    <div>
+      chat pagehc
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import assets from "../assets/assets";
+
+function Login() {
+  return (
+
+    <div className="relative h-screen  bg-background   flex items-center justify-start text-start">
+      <img src={assets.background} alt="background" className="absolute  w-full h-screen  object-cover bg-repeat" />
+      
+    
+    </div>
+
+
+
+  )
+}
+
+export default Login
